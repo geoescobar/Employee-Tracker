75342539da7014ef41c57dadd54d25757f0f11db
George Escobar <geo.escobar214@gmail.com>
2022-05-11 16:49:37 -0500

    server.js inquirer info complete

--- server.js
+++ server.js
@@ -53,29 +53,84 @@ const viewDept = () => {
 };
 
 const viewRoles = () => {
-    console.log('view all roles');
-} 
+  console.log("view all roles");
+};
 
 const viewEmployees = () => {
-    console.log('view all employees');
-}
+  console.log("view all employees");
+};
 
 const addDepartment = () => {
-    console.log('add department');
-}
+    console.log("add department");
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'departmentName',
+            message: 'What is the name of the department?',
+        }
+    ]).then((answers) => {
+        console.log(answers);
+        promptQuestions()
+    })
+  };
 
 const addRole = () => {
-    console.log('add role');
-}
+  console.log("add role");
+  return inquirer.prompt([
+      {
+          type: 'input',
+          name: 'addRoles',
+          message: 'What is the role name?',
+      }, {
+          type: 'input',
+          name: 'salary', 
+          message: 'What is the salary for this role?',
+      }, {
+          type: 'input',
+          name: 'roleDepartment',
+          message: 'What department does this role belong to?',
+      }
+  ]).then((answers) => {
+      console.log(answers);
+      promptQuestions()
+  })
+};
 
 const addEmployee = () => {
-    console.log('add employee');
-}
+  console.log("add employee");
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "What is the employees first name?",
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "What is the employees last name?",
+      },
+      {
+        type: "input",
+        name: "role",
+        message: "What is the employees role?",
+      },
+      {
+        type: "input",
+        name: "manager",
+        message: "Who is the employees manager?",
+      },
+    ])
+    .then((answers) => {
+        console.log(answers);
+      promptQuestions();
+    });
+};
 
 const updateEmployee = () => {
-    console.log('update employees');
-}
+  console.log("update employees");
+};
 
 const quit = () => {
-    console.log('quit');
-}
+  console.log("quit");
+};
